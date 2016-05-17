e6f056ad1375a0c76b63372016b4e440bbac4140
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2016-05-17 14:25:50 -0400

    Added readUnitEnd - not completed

--- COMP123-S2016-Lesson2A/Program.cs
+++ COMP123-S2016-Lesson2A/Program.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
  *Date: May 17th 2016
  *Date Modified: May 17th 2016
  *Description: Advanced Method Demo for Lesson2
- *Versiton : 0.0.3 - Added andXandY method and associated test
+ *Versiton : 0.0.4 - Added readUnitEnd - not completed
  */
 namespace COMP123_S2016_Lesson2A
 {
@@ -34,6 +34,13 @@ namespace COMP123_S2016_Lesson2A
             int result = 0;
          result = addXandY(x, y);
             Console.WriteLine(result);
+            Console.WriteLine();
+
+            addYtoX(ref x, ref y);//pass 50 = +40
+            Console.WriteLine(x);
+            Console.WriteLine();
+
+            Console.WriteLine(readUntilEnd());
         }
 
        /**
@@ -47,7 +54,45 @@ namespace COMP123_S2016_Lesson2A
         {
             int result = 0;
             result = firstNumber + SecondNumber;
-            return result; 
+            return result;
+            
+
+                
         }
+
+       /**
+        * simple method to demonstrate pass by reference with ref Keyword
+        * 
+        * @method addYtoX
+        * @param {ref int} X
+        * @param {ref int} Y
+        * 
+        */
+       public static int addYtoX(ref int X, ref int Y)
+       {
+           X += Y;
+           return X;
+       }
+       public static string[] readUntilEnd()
+       {
+           string[] inputs = new string[50];//never change object
+           int inputCounter = 0;
+         
+
+           do
+           {
+               Console.WriteLine("Enter a value -('end' to stop): ");
+               inputs[inputCounter] = Console.ReadLine();
+               if(inputs[inputCounter]=="end")
+               {
+                   inputCounter = -1;
+               }
+               else
+               {
+                   inputCounter++;
+               }
+           } while (inputCounter !=-1);
+           return inputs;
+       }
     }
 }
